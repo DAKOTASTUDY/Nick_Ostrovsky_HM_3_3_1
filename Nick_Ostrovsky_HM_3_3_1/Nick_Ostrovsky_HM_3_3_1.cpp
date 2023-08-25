56221b522e4bdaf205b1ace507be31dc53771d5f
Nick Ostrovsky <legatus357@gmail.com>
2023-08-25 01:55:10 -0700

    fixed

--- Nick_Ostrovsky_HM_3_3_1/Nick_Ostrovsky_HM_3_3_1.cpp
+++ Nick_Ostrovsky_HM_3_3_1/Nick_Ostrovsky_HM_3_3_1.cpp
@@ -8,16 +8,16 @@ private:
     double num1;
     double num2;
 public:
-    Abaqus() : num1(1), num2(1)
-    {}
+   Abaqus() : num1(1), num2(1)
+   {};
     Abaqus(double num1, double num2)
-    {}
-    bool set_num1(double num1) {
-        if (this->num1 != 0) { this->num1 = num1; return (true); }
+    {};
+    bool set_num1(double nn) {
+        if (nn != 0) { num1 = nn; return (true); }
         else { return (false); }
     }
-    bool set_num2(double num2) {
-        if (this->num2 != 0) { this->num2 = num2; return (true); }
+    bool set_num2(double mm) {
+        if (mm != 0) { num2 = mm; return (true); }
         else { return (false); }
     }
     double get_num1() { return num1; }
@@ -50,30 +50,30 @@ public:
 };
 int main()
 {
-    bool a, b;
-    double n = 10, m = 5;
-    std::cout << "\n Please, enter num1:\t";
-    std::cin >> n;
-    std::cout << "\n Please, enter num2:\t";
-    std::cin >> m;
-    Abaqus calc;
-    a = calc.set_num1(n);
-    b = calc.set_num2(m);
-    // если задаю условие через переменные 
-    if ((calc.set_num1(n)) && calc.set_num2(m))
-    {
-        std::cout << "\n num1 + num2 =" << calc.add() << std::endl;
-        std::cout <<"\n num1 * num2 =" << calc.multiply() << std::endl;
-        std::cout << "\n num1 - num2 =" << calc.substruct_1_2() << std::endl;
-        std::cout << "\n num2 - num1 =" << calc.substruct_2_1() << std::endl;
-        std::cout << "\n num2 / num1 =" << calc.divide_2_1() << std::endl;
-    }
-    else 
-    {
-        std::cout << "Without Zero values, please!\n";
-    }
-    std::cout <<"\n\t\t\t\t\t value boolean" << ((calc.set_num1(n)) && (calc.set_num2(m)));
    
+    double n, m;
+   
+    Abaqus calc;
+    while (1) {
+        std::cout << "\n Please, enter num1:\t";
+        std::cin >> n;
+        std::cout << "\n Please, enter num2:\t";
+        std::cin >> m;
+        if ((calc.set_num1(n) == 1) && (calc.set_num2(m) == 1))
+        {
+            std::cout << "\n num1 + num2 =" << calc.add() << std::endl;
+            std::cout << "\n num1 * num2 =" << calc.multiply() << std::endl;
+            std::cout << "\n num1 - num2 =" << calc.substruct_1_2() << std::endl;
+            std::cout << "\n num2 - num1 =" << calc.substruct_2_1() << std::endl;
+            std::cout << "\n num2 / num1 =" << calc.divide_2_1() << std::endl;
+        }
+        else
+        {
+            std::cout << "Without Zero values, please!\n";
+
+        }
+
+    }
     return (0);
 
 }
